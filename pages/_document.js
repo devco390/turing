253f32c99e917f43e50d1132ca5da93655f74a99
devco390 <danieling90@hotmail.com>
2023-04-29 16:11:24 -0500

    adjustment in fav icon

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ export default function Document() {
 
                 <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
 
-                <link rel="shortcut icon" type="image/icon" href="favicon.png" />
+                <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
 
                 <link rel="stylesheet" href="assets/css/font-awesome.min.css" />
 

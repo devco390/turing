f2abef9ad06c3d718d646230148fe0166c60749c
devco390 <danieling90@hotmail.com>
2023-04-29 11:47:26 -0500

    add Montserrat font

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,8 @@ export default function Document() {
 
                 <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet" />
 
+                <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
+
                 <link rel="shortcut icon" type="image/icon" href="favicon.png" />
 
                 <link rel="stylesheet" href="assets/css/font-awesome.min.css" />

da8f1e6284b4081fefa960cd800b87fb93bae61f
daniel-p-meli <daniel.perez@mercadolibre.com.co>
2023-05-05 19:52:44 -0500

    fix: use Playfair Display font in hero

--- pages/_document.js
+++ pages/_document.js
@@ -4,13 +4,12 @@ export default function Document() {
     return (
         <Html>
             <Head>
-                <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i,900,900i" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,500,600,700,700i,900,900i" rel="stylesheet" />
 
                 <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
 
                 <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet" />
 
-                <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
 
                 <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
 

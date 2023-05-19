210cba5954cc0de3766f89c14d84d1808d5eb81a
daniel-p-meli <daniel.perez@mercadolibre.com.co>
2023-05-18 19:59:34 -0500

    optimize fonts ans styles

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,3 +1,8 @@
+import '../styles/animate.min.css'
+import '../styles/owl.carousel.min.css'
+import '../styles/owl.theme.default.min.css'
+import '../styles/bootstrap.min.css'
+
 import '../styles/style.css'
 import '../styles/responsive.css'
 import '../styles/loading.css'
--- pages/_document.js
+++ pages/_document.js
@@ -7,24 +7,13 @@ export default function Document() {
 
                 <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
 
-                <link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css" />
-
-                <link rel="stylesheet" href="assets/css/animate.css" />
-
-                <link rel="stylesheet" href="assets/css/owl.carousel.min.css" />
-                <link href="assets/css/owl.theme.default.min.css" rel="stylesheet" />
-
-                <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
-
-                <link rel="stylesheet" href="assets/css/responsive.css" />
-
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 
-                <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@600;700&display=swap" rel="stylesheet" />
-                <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400,400i,500,600,700&display=swap" rel="stylesheet" />
-                <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400,400i,500,600,700&display=swap" rel="stylesheet" />
-                <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400,400i,500,600,700&display=swap" rel="stylesheet" />
+                <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
+                <link rel="dns-prefetch" href="https://fonts.gstatic.com" />
+
+                <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&family=Lato:wght@400;700&family=Playfair+Display:wght@400;500;600;700&family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
             </Head>
             <body>
                 <Main />

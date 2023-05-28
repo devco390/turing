57d77bdbfee491edb3bb4e51f28e0454e69280b5
daniel-p-meli <daniel.perez@mercadolibre.com.co>
2023-05-28 18:18:31 -0500

    add google analytics 4 tag

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from "next/script";
 
 export default function Document() {
+
+    const GA_MEASUREMENT_ID = 'G-6908SVZHZ3';
+
     return (
         <Html lang="es">
             <Head>
@@ -14,6 +18,16 @@ export default function Document() {
                 <link rel="dns-prefetch" href="https://fonts.gstatic.com" />
 
                 <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700&family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
+
+                <Script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`} />
+                <Script>
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){window.dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', '${GA_MEASUREMENT_ID}');
+                    `}
+                </Script>
             </Head>
             <body>
                 <Main />

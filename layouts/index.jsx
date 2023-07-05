b38d4b0dc8d938387367a03535c06858e68dd15f
daniel-p-meli <daniel.perez@mercadolibre.com.co>
2023-07-04 19:14:23 -0500

    hide services / show brands

--- layouts/index.jsx
+++ layouts/index.jsx
@@ -51,7 +51,7 @@ const Layout = ({ data }) => {
             <div id="home"></div>
             <Menu data={data.menu} callToActions={data.callToActions} />
             <HeaderSliderArea data={data.headerSliderArea} />
-            <WeDo data={data.weDo} />
+            {/* <WeDo data={data.weDo} /> */}
             <Statistics data={data.statistics} />
             <Gallery data={data.gallery} />
             <Contact data={data.contact} />
